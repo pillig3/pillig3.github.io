ffa18b523980869109a1f6d2e91e78b5aa7927b8
Peter Illig <illigp@carleton.edu>
2018-01-10 21:45:35 -0600

    nice submission form :)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 function playerMove(){
   alert("nice");
 }
+
+function submitForm(){
+  s = document.getElementById("submission").value;
+  if (s.toLowerCase() === "yes".toLowerCase()) {
+    document.getElementById("response").innerHTML = "Glad you like it! me too :)"
+  }
+}
